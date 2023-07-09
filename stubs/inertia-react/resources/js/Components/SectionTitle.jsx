120c9cc2a37a42a5545cf0c7bba4febb6c20d78b
Josh Salway <josh.salway@gmail.com>
2023-07-09 20:34:22 +1000

    Add components

---
+++ stubs/inertia-react/resources/js/Components/SectionTitle.jsx
@@ -0,0 +1,17 @@
+export default function SectionTitle({ children }) {
+    return (
+        <div className="md:col-span-1 flex justify-between">
+            <div className="px-4 sm:px-0">
+                <h3 className="text-lg font-medium text-gray-900 dark:text-gray-100">
+                    {children.title}
+                </h3>
+                <p className="mt-1 text-sm text-gray-600 dark:text-gray-400">
+                    {children.description}
+                </p>
+            </div>
+            <div className="px-4 sm:px-0">
+                {children.aside}
+            </div>
+        </div>
+    );
+}
